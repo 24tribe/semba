824b5001a6731ae2ce8ebc288244c1e49b6969ea
gyubid <245973647+gyubid@users.noreply.github.com>
2026-03-14 19:10:57 -0300

    Fix test wrapper in linux

--- test_wrapper.c
+++ test_wrapper.c
@@ -2,11 +2,27 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+
+#ifdef __linux__
+static int call_exe(char *exe) {
+    size_t len = strlen(exe);
+    char *final_cmd = malloc(len + 2 + 1);
+    if (!final_cmd) { return 1; }
+    strcpy(final_cmd, "./");
+    strcpy(final_cmd + 2, exe);
+    return system(final_cmd);
+}
+#else
+static int call_exe(char *exe) {
+    return system(exe)
+}
+#endif
 
 int main(int argc, char *argv[]) {
     if (argc != 2) {
         fprintf(stderr, "Usage: test_wrapper cmd\n");
         return 1;
     }
-    return system(argv[1]);
+    return call_exe(argv[1]);
 }
