570b7e527a0db1cc9a4e788b18d2a214e4a20695
gyubid <245973647+gyubid@users.noreply.github.com>
2026-03-29 17:31:39 -0300

    Fix identation

--- tests/test_adventure.nim
+++ tests/test_adventure.nim
@@ -313,65 +313,65 @@ proc test_talk_to_miu_after_enonki_read_sequence(saves_dir: string) =
 
 
 proc sameReward(r1: Reward, r2: Reward): bool =
-    result = r1.`type` == r2.`type` and r1.id == r2.id and r1.quantity == r2.quantity
+  result = r1.`type` == r2.`type` and r1.id == r2.id and r1.quantity == r2.quantity
 
 
 proc testAcquireAreaItemInLogs() =
-    var ctx = getInMemorySembaCtx()
+  var ctx = getInMemorySembaCtx()
 
-    let res = ctx.sembaCall("/adventure/acquire_area_item", %*{"areaItemId": 10500102})
+  let res = ctx.sembaCall("/adventure/acquire_area_item", %*{"areaItemId": 10500102})
 
-    doAssert(res != nil)
+  doAssert(res != nil)
 
-    let rewards = to(res["rewards"], seq[Rewards])
+  let rewards = to(res["rewards"], seq[Rewards])
 
-    doAssert(rewards.len == 1)
+  doAssert(rewards.len == 1)
 
-    let firstRewards = rewards[0]
+  let firstRewards = rewards[0]
 
-    doAssert(firstRewards.`type`.get(0) == 5)
+  doAssert(firstRewards.`type`.get(0) == 5)
 
-    let contents = firstRewards.contents
+  let contents = firstRewards.contents
 
-    doAssert(contents.len in {3, 4}) # the forth one is random
+  doAssert(contents.len in {3, 4}) # the forth one is random
 
-    let reward1 = Reward(`type`: 3, id: 1, quantity: 1000)
-    var r1Found = false
+  let reward1 = Reward(`type`: 3, id: 1, quantity: 1000)
+  var r1Found = false
 
-    let reward2 = Reward(`type`: 13, id: 1, quantity: 100)
-    var r2Found = false
+  let reward2 = Reward(`type`: 13, id: 1, quantity: 100)
+  var r2Found = false
 
-    let reward3 = Reward(`type`: 7, id: 2, quantity: 500)
-    var r3Found = false
+  let reward3 = Reward(`type`: 7, id: 2, quantity: 500)
+  var r3Found = false
 
-    for reward in contents:
-        if sameReward(reward, reward1):
-            r1Found = true
+  for reward in contents:
+    if sameReward(reward, reward1):
+      r1Found = true
 
-        if sameReward(reward, reward2):
-            r2Found = true
+    if sameReward(reward, reward2):
+      r2Found = true
 
-        if sameReward(reward, reward3):
-            r3Found = true
+    if sameReward(reward, reward3):
+      r3Found = true
 
-    doAssert(r1Found and r2Found and r3Found)
+  doAssert(r1Found and r2Found and r3Found)
 
 
 proc testAcquireAreaItemNotInLogs() =
-    var ctx = getInMemorySembaCtx()
+  var ctx = getInMemorySembaCtx()
 
-    let res = ctx.sembaCall("/adventure/acquire_area_item", %*{"areaItemId": 10519701})
+  let res = ctx.sembaCall("/adventure/acquire_area_item", %*{"areaItemId": 10519701})
 
-    doAssert(res != nil)
+  doAssert(res != nil)
 
 
 when isMainModule:
-    let saves_dir = paramStr(1)
+  let saves_dir = paramStr(1)
 
-    test_talk_with_enoki_first(saves_dir)
-    test_talk_to_miu_after_enonki_read_sequence(saves_dir)
-    test_talk_hoimi_read_sequence(saves_dir)
-    test_talk_to_branch_manager_after_hoimi_read_sequence(saves_dir)
+  test_talk_with_enoki_first(saves_dir)
+  test_talk_to_miu_after_enonki_read_sequence(saves_dir)
+  test_talk_hoimi_read_sequence(saves_dir)
+  test_talk_to_branch_manager_after_hoimi_read_sequence(saves_dir)
 
-    testAcquireAreaItemInLogs()
-    testAcquireAreaItemNotInLogs()
+  testAcquireAreaItemInLogs()
+  testAcquireAreaItemNotInLogs()
