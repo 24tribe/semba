b78c569e17f35b1746cb778f7da5353772defa6d
gyubid <245973647+gyubid@users.noreply.github.com>
2026-03-29 17:37:01 -0300

    Get area item rewards from masterdata

--- model_stable/area_item.nim
+++ model_stable/area_item.nim
@@ -1,8 +1,11 @@
 import std/json
+import std/options
+import std/random
 
 import ../db_connector/db_sqlite
 
 import ../semba_error
+import ../model_stable/reward
 
 
 type AreaItemContentType* = enum
@@ -11,11 +14,55 @@ type AreaItemContentType* = enum
   itemContentType = 7,
   charExpContentType = 13
 
+type MdQuantityLottery = object
+  quantity: int
 
-proc getAreaItemRewards*(db: DbConn, areaItemId: int): JsonNode =
-  let row = db.getRow(sql"SELECT rewards FROM areaItemRewards WHERE areaItemId = ?", areaItemId);
+type MdQuantityLotteryReward = object
+  quantity_lotteries: seq[MdQuantityLottery]
+  reward_id: int
+  reward_type: int
+
+type MdAreaItemReward = object
+  id: int
+  quantity_lottery_reward: Option[MdQuantityLotteryReward]
+
+
+proc getAreaItemRewardIds(db: DbConn, areaItemId: int): seq[int] =
+  let row = db.getRow(sql"SELECT areaItemRewardIds FROM mdAreaItem WHERE id = ?", areaItemId)
 
   if row[0] == "":
-    raise newException(SembaError, "Couldn't find rewards for areaItemId=" & $areaItemId)
+    raise newException(SembaError, "Couldn't find reward ids for areaItemId=" & $areaItemId)
 
-  return parseJson(row[0])
+  result = to(parseJson(row[0]), seq[int])
+
+
+proc getMdAreaItemReward(db: DbConn, areaItemRewardId: int): MdAreaItemReward = 
+  let row = db.getRow(sql"SELECT id, quantityLotteryReward FROM mdAreaItemReward WHERE id = ?", areaItemRewardId)
+
+  if row[0] == "":
+    raise newException(SembaError, "Couldn't find areaItemReward for areaItemRewardId=" & $areaItemRewardId)
+
+  result.id = areaItemRewardId
+
+  if row[1] != "":
+    result.quantityLotteryReward = some(to(parseJson(row[1]), MdQuantityLotteryReward))
+
+
+proc getAreaItemRewards*(db: DbConn, areaItemId: int): seq[Rewards] =
+  let areaItemRewardIds = getAreaItemRewardIds(db, areaItemId)
+
+  var rewards = newSeq[Reward]()
+
+  for areaItemRewardId in areaItemRewardIds:
+    let reward = getMdAreaItemReward(db, areaItemRewardId)
+    if reward.quantityLotteryReward.isSome():
+      let quantityLotteryReward = reward.quantityLotteryReward.get()
+      let quantityLottery = sample(quantityLotteryReward.quantityLotteries)
+      if quantityLottery.quantity != 0:
+        rewards.add(Reward(
+          `type`: quantityLotteryReward.rewardType,
+          id: quantityLotteryReward.rewardId,
+          quantity: quantityLottery.quantity,
+        ))
+
+  result.add(Rewards(`type`: some(5), contents: rewards))
