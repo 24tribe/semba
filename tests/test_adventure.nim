698b942ae18f12d64b0b5491cd6bdb8a8c277358
gyubid <245973647+gyubid@users.noreply.github.com>
2026-03-29 16:04:48 -0300

    Improve test

--- tests/test_adventure.nim
+++ tests/test_adventure.nim
@@ -1,10 +1,57 @@
 import std/json
 import std/assertions
+import std/options
 
 import test_semba
+import ../model_stable/reward
 
 
-proc testAcquireAreaItem() =
+proc sameReward(r1: Reward, r2: Reward): bool =
+    result = r1.`type` == r2.`type` and r1.id == r2.id and r1.quantity == r2.quantity
+
+
+proc testAcquireAreaItemInLogs() =
+    var ctx = getInMemorySembaCtx()
+
+    let res = ctx.sembaCall("/adventure/acquire_area_item", %*{"areaItemId": 10500102})
+
+    doAssert(res != nil)
+
+    let rewards = to(res["rewards"], seq[Rewards])
+
+    doAssert(rewards.len == 1)
+
+    let firstRewards = rewards[0]
+
+    doAssert(firstRewards.`type`.get(0) == 5)
+
+    let contents = firstRewards.contents
+
+    doAssert(contents.len in {3, 4}) # the forth one is random
+
+    let reward1 = Reward(`type`: 3, id: 1, quantity: 1000)
+    var r1Found = false
+
+    let reward2 = Reward(`type`: 13, id: 1, quantity: 100)
+    var r2Found = false
+
+    let reward3 = Reward(`type`: 7, id: 2, quantity: 500)
+    var r3Found = false
+
+    for reward in contents:
+        if sameReward(reward, reward1):
+            r1Found = true
+
+        if sameReward(reward, reward2):
+            r2Found = true
+
+        if sameReward(reward, reward3):
+            r3Found = true
+
+    doAssert(r1Found and r2Found and r3Found)
+
+
+proc testAcquireAreaItemNotInLogs() =
     var ctx = getInMemorySembaCtx()
 
     let res = ctx.sembaCall("/adventure/acquire_area_item", %*{"areaItemId": 10519701})
@@ -13,4 +60,5 @@ proc testAcquireAreaItem() =
 
 
 when isMainModule:
-    testAcquireAreaItem()
+    testAcquireAreaItemInLogs()
+    testAcquireAreaItemNotInLogs()
