a9b7d76f4c57bf03505b24f80429592b7ed20aa9
gyubid <245973647+gyubid@users.noreply.github.com>
2026-03-30 19:00:43 -0300

    Add test

--- tests/test_extra.nim
+++ tests/test_extra.nim
@@ -13,5 +13,15 @@ proc test_null() =
   doAssert(row[1] == "10")
 
 
+proc test_bool_is_not_zero_or_one() =
+  let db = initMemoryDb()
+  db.exec(sql"CREATE TABLE asd (x BOOLEAN)")
+  db.exec(sql"INSERT INTO asd (x) VALUES (?)", true)
+
+  let row = db.getRow(sql"SELECT x FROM asd")
+  doAssert(row[0] == "true")
+
+
 when isMainModule:
   test_null()
+  test_bool_is_not_zero_or_one()
