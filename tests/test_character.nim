c34a91d3b758e551cad7310532f23f20e161ca01
gyubid <245973647+gyubid@users.noreply.github.com>
2026-03-30 15:21:37 -0300

    Add a failing test

--- tests/test_character.nim
+++ tests/test_character.nim
@@ -1,8 +1,28 @@
 import std/assertions
+import std/json
+import std/options
+
+import ../model_stable/resources
+import ../model_stable/character
+import utils
 
 
 proc testCharacterEquip() =
-    doAssert(false)
+    var ctx = getInMemorySembaCtx()
+
+    let gearId = 10000
+    let res = ctx.sembaCall("/character/equip", %*{ "characterId": 101101, "gearSlot3": gearId })
+
+    doAssert(res != nil)
+
+    let crRes = to(res, ChangedResourcesResponse)
+
+    let characters = to(%*(crRes.changedResources.characters.get()), seq[Character])
+
+    doAssert(characters.len == 1)
+    doAssert(characters[0].gearSlot3.get() == gearId)
+
+    # TODO: should check status updates?
 
 
 when isMainModule:
