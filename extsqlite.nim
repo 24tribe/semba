3b9466da907d1d09bcb59ac215ac1320834c66f3
gyubid <245973647+gyubid@users.noreply.github.com>
2026-03-30 16:41:05 -0300

    Encode an "isNone" Option as an empty string

--- extsqlite.nim
+++ extsqlite.nim
@@ -1,6 +1,9 @@
+import std/options
+
 import db_connector/sqlite3
 
 type ExtSqliteError = object of CatchableError
+
 
 proc loadSql*(db: PSqlite3, sql: string) =
     var err: cstring = nil
@@ -11,3 +14,7 @@ proc loadSql*(db: PSqlite3, sql: string) =
         let errStr = $err
         sqlite3.free(err)
         raise newException(ExtSqliteError, "loadSql failed: " & errStr)
+
+
+proc optionToSqlArg*[T](val: Option[T]): string =
+    if val.isSome(): $val.get() else: ""
