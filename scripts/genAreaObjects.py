0ba3742b9c9227bffd19d5b741fa0bcbd95b63ee
gyubid <245973647+gyubid@users.noreply.github.com>
2026-03-30 10:42:52 -0300

    Split code in functions

--- scripts/genAreaObjects.py
+++ scripts/genAreaObjects.py
@@ -2,6 +2,7 @@ from argparse import ArgumentParser
 import json
 import sys
 
+
 def main():
     parser = ArgumentParser()
     parser.add_argument("online_logs_json")
@@ -28,41 +29,20 @@ def main():
             if flow["res"] is not None: # why areaId=800010 res is empty?
                 write_sql(f, flow["res"], flow["req"]["areaId"])
 
+
 def write_sql(f, data, area_id):
     print(f"-- Area {area_id}", file=f)
     # enemies
     enemies = list(filter(lambda obj: "areaEnemyRateSetId" in obj, data["areaObjects"]))
 
     if enemies:
-        print("INSERT INTO areaEnemies (areaId, areaPointId, areaEnemyRateSetId, action)", file=f)
-        print("VALUES", file=f)
-        first = True
-        for obj in enemies:
-            if first:
-                first = False
-            else:
-                f.write(",")
-
-            print(f"({area_id}, {obj["areaPointId"]}, {obj["areaEnemyRateSetId"]}, '{json.dumps(obj["action"])}')", file=f)
-        
-        print(";", file=f)
+        write_enemies(f, enemies, area_id)
 
     write_area_objects_sql(f, data, area_id)
 
     if "areaItems" in data:
-        # area items
-        print("INSERT INTO areaItems (areaId, areaItemId)", file=f)
-        print("VALUES", file=f)
+        write_area_items(f, data, area_id)
 
-        first = True
-        for obj in data["areaItems"]:
-            if first:
-                first = False
-            else:
-                f.write(",")
-
-            print(f"({area_id}, {obj["areaItemId"]})", file=f)
-        print(";", file=f)
 
 def write_area_objects_sql(f, data, area_id):
     area_objects = list(filter(lambda obj: "areaObjectId" in obj, data["areaObjects"]))
@@ -89,6 +69,38 @@ def write_area_objects_sql(f, data, area_id):
         print(f"({area_id}, {obj["areaObjectId"]}, {obj["areaPointId"]}, {obj["areaObjectBehaviorId"]}, {action})", file=f)
 
     print(";", file=f)
+
+
+def write_enemies(f, enemies, area_id):
+    print("INSERT INTO areaEnemies (areaId, areaPointId, areaEnemyRateSetId, action)", file=f)
+    print("VALUES", file=f)
+    first = True
+    for obj in enemies:
+        if first:
+            first = False
+        else:
+            f.write(",")
+
+        print(f"({area_id}, {obj["areaPointId"]}, {obj["areaEnemyRateSetId"]}, '{json.dumps(obj["action"])}')", file=f)
+
+    print(";", file=f)
+
+
+def write_area_items(f, data, area_id):
+    # area items
+    print("INSERT INTO areaItems (areaId, areaItemId)", file=f)
+    print("VALUES", file=f)
+
+    first = True
+    for obj in data["areaItems"]:
+        if first:
+            first = False
+        else:
+            f.write(",")
+
+        print(f"({area_id}, {obj["areaItemId"]})", file=f)
+    print(";", file=f)
+
 
 if __name__ == "__main__":
     main()
