a3608bf3223b55aab9c58371f03d17850aa2429c
gyubid <245973647+gyubid@users.noreply.github.com>
2026-03-31 14:41:11 -0300

    Randomize gear and substats from chests

--- model_stable/gear.nim
+++ model_stable/gear.nim
@@ -1,6 +1,8 @@
 import std/options
 import std/strutils
 import std/json
+import std/random
+import std/sequtils
 
 import ../db_connector/db_sqlite
 
@@ -9,6 +11,8 @@ import ../semba_error
 import timestamp
 import reward
 import entity
+import city
+import character
 
 
 type GearRarity* = enum
@@ -28,6 +32,22 @@ type Gear* = object
   subStatus2Id*: Option[int]
   subStatus3Id*: Option[int]
   trainingScoreLevelScore*: Option[int]
+
+type MdGear = object
+  id: int
+  grade: int
+  gearTypeId: int
+  descr: string
+  compressItemRewards: JsonNode # FIXME: proper type
+  compressItems: JsonNode # FIXME: proper type
+
+
+type MdGearStatus = object
+  id: int
+  rarity: int
+  statusEffectType: Option[int]
+  statusEffectValue: Option[float]
+  statusGroupId: int
 
 
 proc addGear*(db: DbConn, gear: Gear) =
@@ -73,38 +93,132 @@ proc getGears*(db: DbConn): seq[Gear] =
     result.add(getGear(db, parseInt(row[0])))
 
 
-proc getGearReward(db: DbConn): Reward = 
-  result = Reward(
-    `type`: rewardGear.int,
-    id: 30001201,
-    quantity: 1,
-    entityId: some(popEntityId(db)),
-    resourceParams: some(to(%*{
-      "gearRewardStatus": {
-        "subStatusIds": [
-          11011006,
-          10099001,
-          10021002
-        ],
-        "gearRarity": 4
-      }
-    }, ResourceParams)),
-    isNew: some(true),
+proc getMdGears(db: DbConn, descr: string): seq[MdGear] =
+  let rows = db.getAllRows(sql"""
+    SELECT id, grade, gearTypeId, compressItemRewards, compressItems FROM mdGear
+    WHERE descr = ?
+  """, descr)
+
+  for row in rows:
+    result.add(MdGear(
+      id: parseInt(row[0]),
+      grade: parseInt(row[1]),
+      gearTypeId: parseInt(row[2]),
+      descr: descr,
+      compressItemRewards: parseJson(row[3]),
+      compressItems: parseJson(row[4]),
+    ))
+
+
+proc getMdGearStatsWithRarity(db: DbConn, rarity: int): seq[MdGearStatus] =
+  let rows = db.getAllRows(sql"""
+    SELECT id, statusEffectType, statusEffectValue, statusGroupId FROM mdGearStatus
+    WHERE rarity = ?
+  """, rarity)
+
+  for row in rows:
+    result.add(MdGearStatus(
+      id: parseInt(row[0]),
+      rarity: rarity,
+      statusEffectType: tryParseInt(row[1]),
+      statusEffectValue: tryParseFloat(row[2]),
+      statusGroupId: parseInt(row[3]),
+    ))
+
+
+proc getMdGearStatsWithMaxRarity(db: DbConn, maxRarity: int): seq[MdGearStatus] =
+  let rows = db.getAllRows(sql"""
+    SELECT id, rarity, statusEffectType, statusEffectValue, statusGroupId FROM mdGearStatus
+    WHERE rarity <= ?
+  """, maxRarity)
+
+  for row in rows:
+    result.add(MdGearStatus(
+      id: parseInt(row[0]),
+      rarity: parseInt(row[1]),
+      statusEffectType: tryParseInt(row[2]),
+      statusEffectValue: tryParseFloat(row[3]),
+      statusGroupId: parseInt(row[4]),
+    ))
+
+
+proc getBalancedGears(db: DbConn): seq[MdGear] =
+  let maxLevel = getCharacterMaxLevel(db)
+
+  case maxLevel:
+  of 10:
+    result = getMdGears(db, "grade 1 - Shinagawa")
+  of 15:
+    result = getMdGears(db, "grade 2 - Shinagawa")
+  else:
+    result = getMdGears(db, "grade 3 - Shinagawa") # FIXME: minato, chiyoda, fv???
+
+
+proc getRandomSubstatWithMaxRarity(db: DbConn, maxRarity: int): MdGearStatus =
+  result = getMdGearStatsWithMaxRarity(db, maxRarity).sample()
+
+
+proc getRandomSubstatWithRarity(db: DbConn, rarity: int): MdGearStatus =
+  result = getMdGearStatsWithRarity(db, rarity).sample()
+
+
+proc getSubstats(db: DbConn, rarity: int): (Option[MdGearStatus], Option[MdGearStatus], Option[MdGearStatus]) =
+  var subStatus1: Option[MdGearStatus]
+  var subStatus2: Option[MdGearStatus]
+  var subStatus3: Option[MdGearStatus]
+
+  if rarity != gearRarityN.int:
+    subStatus1 = some(getRandomSubstatWithRarity(db, rarity))
+
+  if rarity == gearRaritySr.int or rarity == gearRaritySsr.int:
+    subStatus2 = some(getRandomSubstatWithMaxRarity(db, rarity))
+
+  if rarity == gearRaritySsr.int:
+    subStatus3 = some(getRandomSubstatWithMaxRarity(db, gearRaritySsr.int))
+
+  result = (subStatus1, subStatus2, subStatus3)
+
+
+proc randomGear*(db: DbConn, minRarity: int): (Gear, Reward) =
+  let mdGears = getBalancedGears(db)
+
+  let mdGear = mdGears.sample()
+
+  let rarity = toSeq(minRarity .. gearRaritySsr.int).sample()
+
+  let (subStatus1, subStatus2, subStatus3) = getSubstats(db, rarity)
+
+  let entityId = popEntityId(db)
+
+  let subStatusIds = (
+    [subStatus1, subStatus2, subStatus3]
+    .filter(proc (x: Option[MdGearStatus]): bool = x.isSome())
+    .map(proc (x: Option[MdGearStatus]): int = x.get().id)
+  ) 
+
+  let gearRewardStatus = GearRewardStatus(
+    subStatusIds: some(subStatusIds),
+    gearRarity: rarity
   )
-
-
-proc randomGear*(db: DbConn): (Gear, Reward) =
-  let reward = getGearReward(db)
+  
+  let reward = Reward(
+    `type`: rewardGear.int,
+    id: mdGear.id,
+    quantity: 1,
+    entityId: some(entityId),
+    resourceParams: some(ResourceParams(gearRewardStatus: some(gearRewardStatus))),
+    isNew: some(true),
+  ) 
 
   let gear = Gear(
-    entityId: reward.entityId.get(),
-    gearId: reward.id,
+    entityId: entityId,
+    gearId: mdGear.id,
     receivedAt: getTimestampNow(),
-    subStatus1Id: some(reward.resourceParams.get().gearRewardStatus.get().subStatusIds.get()[0]),
-    subStatus2Id: some(reward.resourceParams.get().gearRewardStatus.get().subStatusIds.get()[1]),
-    subStatus3Id: some(reward.resourceParams.get().gearRewardStatus.get().subStatusIds.get()[2]),
+    subStatus1Id: subStatus1.map(proc (x: MdGearStatus): int = x.id),
+    subStatus2Id: subStatus2.map(proc (x: MdGearStatus): int = x.id),
+    subStatus3Id: subStatus3.map(proc (x: MdGearStatus): int = x.id),
     trainingScoreLevelScore: some(1),
-    rarity: reward.resourceParams.get().gearRewardStatus.get().gearRarity
+    rarity: rarity
   )
 
   result = (gear, reward)
