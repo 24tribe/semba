521477c75d95d64932bc6978e1837fd7e03a7b55
gyubid <245973647+gyubid@users.noreply.github.com>
2026-04-02 17:16:01 -0300

    Add failing test

--- tests/test_character.nim
+++ tests/test_character.nim
@@ -9,11 +9,13 @@ import ../model_stable/gear
 import ../model_stable/timestamp
 import utils
 
+const yoCharId = 100101
+const irohaCharId = 100201
+const koishiCharId = 100501
 
 proc testCharacterStatsDependOnLevel() =
   var ctx = getInMemorySembaCtx()
 
-  const yoCharId = 100101
   const yoLevel2MaxHp = 511
   const yoLevel2Attack = 106
   const yoLevel2Defense = 105
@@ -26,7 +28,6 @@ proc testCharacterStatsDependOnLevel() =
       character.attack.get(0) == yoLevel2Attack and
       character.defense.get(0) == yoLevel2Defense
 
-  const irohaCharId = 100201
   const irohaLevel1MaxHp = 452
   const irohaLevel1Attack = 109
   const irohaLevel1Defense = 92
@@ -89,9 +90,93 @@ proc testCharacterEquip() =
   doAssert(characters2[0].gearSlot2.isNone())
   doAssert(characters2[0].gearSlot3.isNone())
 
-  # TODO: should check status updates?
+
+proc checkStats(original: Character, newChar: Character) =
+  doAssert(original.exp == newChar.exp)
+  doAssert(original.attack == newChar.attack)
+  doAssert(original.defense == newChar.defense)
+  doAssert(original.maxHp == newChar.maxHp)
+  doAssert(original.criticalRate == newChar.criticalRate)
+  doAssert(original.criticalDamageRate == newChar.criticalDamageRate)
+
+
+proc testCharacterGearStats() =
+  var ctx = getInMemorySembaCtx()
+
+  let gears = to(%*[{
+    "entityId": 1, "gearId": 10001101, "receivedAt": "2025-10-13T20:51:23Z",
+    "subStatus1Id": 10041001, "subStatus2Id": 10020002,
+    "trainingScoreLevelScore": 1, "rarity": 3
+  }, {
+    "entityId": 2, "gearId": 10001101, "receivedAt": "2025-10-13T20:53:54Z",
+    "subStatus1Id": 10081001, "subStatus2Id": 10010001,
+    "trainingScoreLevelScore": 1, "rarity": 3
+  }, {
+    "entityId": 3, "gearId": 30001201, "receivedAt": "2025-10-13T20:55:32Z",
+    "subStatus1Id": 10080001, "subStatus2Id": 10098001,
+    "trainingScoreLevelScore": 1, "rarity": 3
+  }, {
+    "entityId": 5, "gearId": 30001101, "receivedAt": "2025-10-13T21:02:51Z",
+    "subStatus1Id": 10060002, "subStatus2Id": 10050001,
+    "trainingScoreLevelScore": 1, "rarity": 3
+  }, {
+    "entityId": 6, "gearId": 20001201, "receivedAt": "2025-10-13T21:05:48Z",
+    "subStatus1Id": 11012003, "subStatus2Id": 10060002, "subStatus3Id": 10010001,
+    "trainingScoreLevelScore": 1, "rarity": 4
+  }, {
+    "entityId": 7, "gearId": 30001001, "receivedAt": "2025-10-13T21:06:47Z",
+    "subStatus1Id": 11006004, "subStatus2Id": 10061002, "subStatus3Id": 10050001,
+    "trainingScoreLevelScore": 1, "rarity": 4
+  }], seq[Gear])
+
+  for gear in gears:
+    addGear(ctx.db, gear)
+
+  var yo = getCharacter(ctx.db, yoCharId)
+  yo.exp = some(2797)
+  yo.gearSlot2 = some(2)
+  yo.gearSlot3 = some(6)
+
+  var iroha = getCharacter(ctx.db, irohaCharId)
+  iroha.exp = some(2745)
+  iroha.gearSlot1 = some(7)
+  iroha.gearSlot2 = some(1)
+  iroha.gearSlot3 = some(3)
+
+  var koishi = getCharacter(ctx.db, koishiCharId)
+  iroha.exp = some(2745)
+  iroha.gearSlot2 = some(5)
+
+  updateCharacters(ctx.db, @[%*yo, %*iroha, %*koishi])
+
+  yo.attack = some(147)
+  yo.defense = some(125)
+  yo.maxHp = some(836)
+  yo.criticalDamageRate = some(70.0)
+
+  iroha.attack = some(175)
+  iroha.defense = some(135)
+  iroha.maxHp = some(666)
+  iroha.criticalRate = some(9.5)
+  iroha.criticalDamageRate = some(80.0)
+
+  iroha.attack = some(100)
+  iroha.defense = some(141)
+  iroha.maxHp = some(786)
+  iroha.criticalRate = some(9.5)
+  iroha.criticalDamageRate = some(70.0)
+
+  let yoNew = getCharacter(ctx.db, yoCharId)
+  checkStats(yo, yoNew)
+
+  let irohaNew = getCharacter(ctx.db, irohaCharId)
+  checkStats(iroha, irohaNew)
+
+  let koishiNew = getCharacter(ctx.db, koishiCharId)
+  checkStats(koishi, koishiNew)
 
 
 proc testSuiteCharacter*() =
   testCharacterEquip()
   testCharacterStatsDependOnLevel()
+  testCharacterGearStats()
