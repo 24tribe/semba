d98d7f2f14f1f1a649f8d1dd89f71a4fe908ce77
gyubid <245973647+gyubid@users.noreply.github.com>
2026-04-05 16:43:14 -0300

    Change JsonNode to Wallet in wallet.nim

--- model_stable/wallet.nim
+++ model_stable/wallet.nim
@@ -1,21 +1,23 @@
 import std/json
 import std/strutils
+import std/options
 
 import ../db_connector/db_sqlite
 
 
-proc getWallet*(db: DbConn): JsonNode =
+type Wallet* = object
+  free*: Option[int]
+  paid*: Option[int]
+
+
+proc getWallet*(db: DbConn): Wallet =
   let freeGemsRow = db.getRow(sql"SELECT val FROM userData WHERE keyName='freeGems'")
   let freeGems = parseInt(freeGemsRow[0])
   let paidGemsRow = db.getRow(sql"SELECT val FROM userData WHERE keyName='paidGems'")
   let paidGems = parseInt(paidGemsRow[0])
-  return %*{
-    "free": freeGems,
-    "paid": paidGems
-  }
+  result = Wallet(free: some(freeGems), paid: some(paidGems))
 
-proc setWallet*(db: DbConn, wallet: JsonNode) =
-  let freeGems = wallet["free"].getInt()
-  let paidGems = wallet["paid"].getInt()
-  db.exec(sql"UPDATE userData SET val=? WHERE keyName='freeGems'", $freeGems)
-  db.exec(sql"UPDATE userData SET val=? WHERE keyName='paidGems'", $paidGems)
+
+proc setWallet*(db: DbConn, wallet: Wallet) =
+  db.exec(sql"UPDATE userData SET val=? WHERE keyName='freeGems'", wallet.free.get(0))
+  db.exec(sql"UPDATE userData SET val=? WHERE keyName='paidGems'", wallet.paid.get(0))
