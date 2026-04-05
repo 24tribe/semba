76a1892a98edbd5449f2a8fdfb1f734d8aeedd80
gyubid <245973647+gyubid@users.noreply.github.com>
2026-04-05 16:24:49 -0300

    Store graffitiArts in db and add failing test

---
+++ tests/test_graffiti_art.nim
@@ -0,0 +1,35 @@
+import std/json
+import std/options
+
+import utils
+import ../api_stable/adventure
+import ../model_stable/reward
+import ../model_stable/graffiti_art
+
+
+proc testFindGraffiti() =
+  var ctx = getInMemorySembaCtx()
+
+  let graffitiArtId = 10100201
+
+  let res = to(sembaCall(ctx, "/adventure/find_graffiti", %*{
+    "graffitiArtId": graffitiArtId,
+    "currentLocation": {
+      "areaType": 1, "direction": 1, "positionCoordinates": {"x": -9.4, "y": 0.5, "z": 0.2}, "areaKeyId": 101002
+    }
+  }), Option[AdventureFindGraffitiResponse])
+
+  doAssert(res.isSome())
+
+  let response = res.get()
+
+  doAssert(response.rewards.len == 1)
+  doAssert(response.rewards[0].`type`.RewardType == rewardFreeGem)
+  doAssert(response.rewards[0].quantity == 5)
+
+  doAssert(response.changedResources.wallet.isSome())
+  doAssert(response.changedResources.graffitiArts.get(@[]) == @[GraffitiArt(graffitiArtId: 10130102)])
+
+
+proc testSuiteGraffitiArt*() =
+  testFindGraffiti()
