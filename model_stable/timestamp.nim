240a88c0a1dad5455228532345f81e0e7b8c3581
gyubid <245973647+gyubid@users.noreply.github.com>
2026-04-11 20:10:06 -0300

    Add getFutureTimestamp

--- model_stable/timestamp.nim
+++ model_stable/timestamp.nim
@@ -5,6 +5,7 @@ type Timestamp* = distinct string
 
 proc getDateNow*(): string = $(now().utc)
 proc getTimestampNow*(): Timestamp = getDateNow().Timestamp
+proc getFutureTimestamp*(): Timestamp = "2100-01-11T20:20:25Z".Timestamp
 
 proc `%`*(timestamp: Timestamp): JsonNode {.borrow.}
 proc `==`*(a, b: Timestamp): bool {.borrow.}
