d349cca6017bcc61bc15bf4b30471175ad789ecc
gyubid <245973647+gyubid@users.noreply.github.com>
2026-06-09 16:09:58 -0300

    Fix formatting

--- tests/test_mission.nim
+++ tests/test_mission.nim
@@ -12,81 +12,81 @@ import ../model_stable/timestamp
 import ../model_stable/area_object
 
 proc testMissionReceive() =
-    var ctx = getInMemorySembaCtx()
+  var ctx = getInMemorySembaCtx()
 
-    updateMissions(ctx.db, [
-        Mission(
-            missionId: 1041041, count: some(155),
-            receivedStepCount: some(0), clearedAt: some(getTimestampNow())
-        ),
-        Mission(
-            missionId: 1041042, count: some(155),
-            receivedStepCount: some(0), clearedAt: some(getTimestampNow())
-        ),
-    ])
+  updateMissions(ctx.db, [
+    Mission(
+      missionId: 1041041, count: some(155),
+      receivedStepCount: some(0), clearedAt: some(getTimestampNow())
+    ),
+    Mission(
+      missionId: 1041042, count: some(155),
+      receivedStepCount: some(0), clearedAt: some(getTimestampNow())
+    ),
+  ])
 
-    let missionIds = [1041041, 1041042]
+  let missionIds = [1041041, 1041042]
 
-    let resJson = ctx.sembaCall("/mission/receive", %*{"missionIds": missionIds})
+  let resJson = ctx.sembaCall("/mission/receive", %*{"missionIds": missionIds})
 
-    doAssert(resJson != nil)
+  doAssert(resJson != nil)
 
-    let res = protoJsonTo(resJson, MissionReceiveResponse)
+  let res = protoJsonTo(resJson, MissionReceiveResponse)
 
-    doAssert(res.changedResources.missions.isSome())
+  doAssert(res.changedResources.missions.isSome())
 
-    let missions = res.changedResources.missions.get().mapIt((it.missionId, it)).toTable()
+  let missions = res.changedResources.missions.get().mapIt((it.missionId, it)).toTable()
 
-    for missionId in missionIds:
-        let mission = missions[missionId]
-        doAssert(mission.receivedStepCount.get(0) == 1)
+  for missionId in missionIds:
+    let mission = missions[missionId]
+    doAssert(mission.receivedStepCount.get(0) == 1)
 
-    doAssert(
-        getMissionsWithIds(ctx.db, missionIds).all(proc (mi: Mission): bool = mi.receivedStepCount.get(0) == 1)
-    )
+  doAssert(
+    getMissionsWithIds(ctx.db, missionIds).all(proc (mi: Mission): bool = mi.receivedStepCount.get(0) == 1)
+  )
 
-    doAssert(res.rewards.len > 0)
-    doAssert(res.changedResources.status.get().flowerMark.get(0) == 2)
+  doAssert(res.rewards.len > 0)
+  doAssert(res.changedResources.status.get().flowerMark.get(0) == 2)
 
 
 proc testUnlockFullMarkGates() =
-    let ctx = getInMemorySembaCtx()
+  let ctx = getInMemorySembaCtx()
 
-    proc getGate(): AreaObject =
-        let areaObjects = protoJsonTo(%*getAreaObjectsInArea(ctx.db, 101103), seq[AreaObject])
-        result = areaObjects.filterIt(it.areaPointId == 101103801).toSeq()[0]
+  proc getGate(): AreaObject =
+    let areaObjects = protoJsonTo(%*getAreaObjectsInArea(ctx.db, 101103), seq[AreaObject])
+    result = areaObjects.filterIt(it.areaPointId == 101103801).toSeq()[0]
 
-    let gateBefore = getGate()
+  let gateBefore = getGate()
 
-    doAssert(gateBefore.action.get() == AreaObjectAction(
-        `type`: 3, id: some(1), label: some("Fence"), sequenceId: some(10836901)
-    ))
+  doAssert(gateBefore.action.get() == AreaObjectAction(
+    `type`: 3, id: some(1), label: some("Fence"), sequenceId: some(10836901)
+  ))
 
-    unlockFullMarksGates(ctx.db, 17)
+  unlockFullMarksGates(ctx.db, 17)
 
-    let gateAfter = getGate()
+  let gateAfter = getGate()
 
-    doAssert(gateAfter.action.get() == AreaObjectAction(`type`: 7, id: some(1)))
+  doAssert(gateAfter.action.get() == AreaObjectAction(`type`: 7, id: some(1)))
 
 
 proc testSaveFileWithBuggedAreaObjectLocksIsFixed(saves_dir: string) =
-    var ctx = getInMemorySembaCtx()
+  var ctx = getInMemorySembaCtx()
 
-    ctx.loadSaveFile(saves_dir, "after puzzle")
+  ctx.loadSaveFile(saves_dir, "after puzzle")
 
-    let locks = getAreaObjectLocks(ctx.db)
+  let locks = getAreaObjectLocks(ctx.db)
 
-    doAssert(locks.findIt(it.areaObjectLockId == 10504002) != -1)
+  doAssert(locks.findIt(it.areaObjectLockId == 10504002) != -1)
 
-    let shinagawaTroubleshooterMissionIds = [1041065, 1041066]
+  let shinagawaTroubleshooterMissionIds = [1041065, 1041066]
 
-    let missions = getMissionsWithIds(ctx.db, shinagawaTroubleshooterMissionIds)
+  let missions = getMissionsWithIds(ctx.db, shinagawaTroubleshooterMissionIds)
 
-    doAssert(missions.len == shinagawaTroubleshooterMissionIds.len)
-    doAssert(missions.allIt(it.count == some(1)))
+  doAssert(missions.len == shinagawaTroubleshooterMissionIds.len)
+  doAssert(missions.allIt(it.count == some(1)))
 
 
 proc testSuiteMission*(saves_dir: string) =
-    testMissionReceive()
-    testUnlockFullMarkGates()
-    testSaveFileWithBuggedAreaObjectLocksIsFixed(saves_dir)
+  testMissionReceive()
+  testUnlockFullMarkGates()
+  testSaveFileWithBuggedAreaObjectLocksIsFixed(saves_dir)
