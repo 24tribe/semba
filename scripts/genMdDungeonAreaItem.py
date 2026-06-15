0c5f1f1bb5c67438ae2b2fe45e80956173180c19
gyubid <245973647+gyubid@users.noreply.github.com>
2026-06-15 16:00:42 -0300

    Add masterdata dungeo_area_item.json to db

---
+++ scripts/genMdDungeonAreaItem.py
@@ -0,0 +1,34 @@
+from argparse import ArgumentParser
+import json
+
+from genMasterData import write_rows
+
+def main():
+    parser = ArgumentParser()
+    parser.add_argument("dungeon_area_item_json")
+    parser.add_argument("out_sql")
+    args = parser.parse_args()
+
+    with open(args.dungeon_area_item_json, "r", encoding="utf-8") as f:
+        dungeon_area_item = json.load(f)
+
+    with open(args.out_sql, "w", encoding="utf-8") as f:
+        gen_md_dungeon_area_item(dungeon_area_item, f)
+
+    print("OK")
+
+
+def gen_md_dungeon_area_item(dungeon_area_item, f):
+    xprint = lambda *args: print(*args, file=f)
+
+    xprint("INSERT INTO mdDungeonAreaItem (id, areaItemRewardIds) VALUES")
+
+    write_rows(xprint, f, [
+        (dai["id"], dai["area_item_reward_ids"]) for dai in dungeon_area_item
+    ])
+
+    xprint(";")
+
+
+if __name__ == "__main__":
+    main()
